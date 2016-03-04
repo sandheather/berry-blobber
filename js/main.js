f026cec16202d1b310578f3912f30147b8ff8b42
sandheather <heather.e.sanders22@gmail.com>
2016-03-04 17:13:47 -0500

    input of information

--- js/main.js
+++ js/main.js
@@ -35,6 +35,26 @@ var redCurrant = {
 
 
 // Write your function here, name it `writeBerries`
+var writeBerries = function (berries) {
+  document.write('<h1>Berries</h1>');
+
+  document.write('<ul>');
+
+  berries.forEach(function (berry) {
+    document.write('<li>');
+    document.write('<h2>' + berry.name + '</h2>');
+    document.write('<dl>');
+    document.write('<dt>Grows on a shrub?</dt>');
+    document.write('<dd><i>' + berry.onShrub + '</i></dd>');
+    document.write('<dt>Energy</dt>');
+    document.write('<dd><i>' + berry.energy + 'KJ' + '</i></dd>');
+    document.write('<dt>Carbohydrates</dt>');
+    document.write('<dd><i>' + berry.carbs + 'g' '</i></dd>');
+    document.write('<dt>Protein</dt>');
+    document.write('<dd><i>' + berry.protein + 'g' '</i></dd>');
+
+    document.write('<dd>');
+  }
 
 
 // ##########################################################
